a000d4c973381a1a77462c325357c8028b3eb58c
headibr <50640254+headibr@users.noreply.github.com>
2019-05-15 15:41:10 -0500

    Update spacex.py

--- spacex.py
+++ spacex.py
@@ -9,14 +9,18 @@ import webbrowser
 spacex_url = 'https://api.spacexdata.com/v3/launches/latest'
 response = requests.get(spacex_url)
 response = response.json()
-# mission name
+
+# What is the mission name?
 print('The mission name is: ' + response['mission_name'])
-# launch date local
+
+# What is the local launch date?
 print('The local launch date is: ' + response['launch_date_local'])
-# 1 other piece of info
+
+# What is the rocket name?
 print('The rocket name is: ' + response['rocket']['rocket_name'])
+
 # tell the user, "PRESS ENTER TO ACCESS"
 video_link = input('PRESS ENTER TO ACCESS A VIDEO LINK')
-#print(response['links']['video_link'])
+
 # open a link in browser webbrowser.open()
 webbrowser.open(response['links']['video_link'])
